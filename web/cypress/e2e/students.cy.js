68eb2cdf5cee8528e10958dc2ee461aa2575f698
Cassia Caris <cassia.caris@vericode.com.br>
2023-07-27 12:54:27 -0300

    Desafio Feat: Criação de 3 novos cenários no Students

--- web/cypress/e2e/students.cy.js
+++ web/cypress/e2e/students.cy.js
@@ -2,10 +2,10 @@ import StudentPage from '../support/pages/StudentPage'
 
 import students from '../fixtures/students.json'
 
-describe('alunos', ()=> {
+describe('alunos', () => {
 
-    it('deve poder cadastrar um novo aluno', ()=> {
-        
+    it('deve poder cadastrar um novo aluno', () => {
+
         const student = students.create
 
         cy.task('deleteStudent', student.email)
@@ -16,9 +16,9 @@ describe('alunos', ()=> {
         StudentPage.popup.haveText('Dados cadastrados com sucesso.')
     })
 
-    it('não deve cadastrar com email duplicado', ()=> {
+    it('não deve cadastrar com email duplicado', () => {
         const student = students.duplicate
-        
+
         cy.task('resetStudent', student)
 
         cy.adminLogin()
@@ -28,7 +28,7 @@ describe('alunos', ()=> {
         StudentPage.popup.haveText('O email informado já foi cadastrado!')
     })
 
-    it('deve remover um aluno sem matricula', ()=> {
+    it('deve remover um aluno sem matricula', () => {
         const student = students.remove
 
         cy.task('resetStudent', student)
@@ -42,7 +42,7 @@ describe('alunos', ()=> {
         StudentPage.popup.haveText('Exclusão realizada com sucesso.')
     })
 
-    it.only('todos os campos são obrigatórios', ()=> {
+    it('todos os campos são obrigatórios', () => {
         const student = students.required
         cy.adminLogin()
         StudentPage.goToRegister()
@@ -54,6 +54,53 @@ describe('alunos', ()=> {
         StudentPage.requiredMessage('Peso (em kg)', 'O peso é obrigatório')
         StudentPage.requiredMessage('Altura', 'A altura é obrigatória')
 
-       //cy.contains('label', 'Nome completo').parent().find('span').should('have.text', 'Nome é obrigatório')
+        //cy.contains('label', 'Nome completo').parent().find('span').should('have.text', 'Nome é obrigatório')
     })
+
+    //1º Desafio Bootcamp
+    it('tentar cadastrar um aluno menor de 16 anos', () => {
+        const student = students.minor
+        // Dado que estou logado como Administrador da academia
+        cy.task('deleteStudent', student.email)
+        cy.adminLogin()
+        // E que acessei o formulário de cadastro de alunos
+        StudentPage.goToRegister()
+        // Quando tento cadastrar um aluno com idade igual ou menor a 16 anos
+        StudentPage.submitform(student)
+        // Então o sistema não permite o cadastro, exibindo uma mensagem de alerta no campo idade
+        StudentPage.requiredMessage('Idade', 'A idade mínima para treinar é 16 anos!')
+    })
+
+    it('tentar cadastrar informando peso incorreto', () => {
+        const student = students.lower_weights
+
+        student.forEach((a) => {
+            // Dado que estou logado como Administrador da academia
+            cy.task('deleteStudent', a.email)
+            cy.adminLogin()
+            // E que acessei o formulário de cadastro de alunos
+            StudentPage.goToRegister()
+            // Quando tento cadastrar um aluno informando um peso menor ou igual a ZERO
+            StudentPage.submitform(a)
+            // Então o sistema não permite o cadastro, exibindo uma mensagem de alerta no campo peso
+            StudentPage.requiredMessage('Peso (em kg)', 'O peso mínimo para treinar é 40 Kg')
+        })
+    })
+
+    it('tentar cadastrar informando altura incorreta', () => {
+        const student = students.bottom_feet_tall
+
+        student.forEach((a) => {
+            // Dado que estou logado como Administrador da academia
+            cy.task('deleteStudent', a.email)
+            cy.adminLogin()
+            // E que acessei o formulário de cadastro de alunos
+            StudentPage.goToRegister()
+            // Quando tento cadastrar um aluno informando uma altura menor ou igual a ZERO
+            StudentPage.submitform(a)
+            // Então o sistema não permite o cadastro, exibindo uma mensagem de alerta no campo altura
+            StudentPage.requiredMessage('Altura', 'Não é possível cadastrar uma altura negativa')
+        })
+    })
+
 })
