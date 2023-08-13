ded5bf4e42552ebb630215226cfc263a3deeafb1
Cassia Caris <cassia.caris@vericode.com.br>
2023-08-13 19:02:04 -0300

    Consumindo a API com Cypress

--- web/cypress/e2e/enrollments.cy.js
+++ web/cypress/e2e/enrollments.cy.js
@@ -24,4 +24,48 @@ describe('matriculas', () => {
         EnrollsPage.popup.haveText('Matrícula cadastrada com sucesso.')
     })
 
+    it.only('não deve criar matricula duplicata', () => {
+        const dataTest = data.duplicate
+
+        cy.task('resetStudent', dataTest.student)
+        cy.task('selectStudentId', dataTest.student.email)
+            .then(result => { //realização do callback
+  
+                cy.request({
+                    url: 'http://localhost:3333/sessions',
+                    method: 'POST',
+                    body: {
+                        email: "admin@healthxp.com",
+                        password: "xperience"
+                    }
+                }).then(Response=> {
+                    cy.log(Response.body.token)
+
+                    const payload = { //criado uma cosntate para o payload
+                        student_id: result.success.rows[0].id,
+                        plan_id: dataTest.plan.id,
+                        credit_card: "4242"
+                    }
+
+                    cy.request({ //realizando a criação da matricula
+                        url: 'http://localhost:3333/enrollments',
+                        method: 'POST',
+                        body: payload,
+                        headers: {
+                            Authorization: 'Bearer ' + Response.body.token
+                        }
+                    }).then(Response=> {
+                        expect(Response.status).to.eq(201)
+                    })
+                })
+
+
+            })
+
+
+
+
+
+    })
+
 })
